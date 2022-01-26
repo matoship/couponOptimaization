fb0114c66be90a3ed6ce1c479a78ef34db7a4cf1
kaifeng <chenkaifenshiwo@gmail.com>
2022-01-26 18:35:52 +0800

    the fourth try

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class item {
     int id;
@@ -28,11 +26,17 @@ class item {
 }
 
 class coupon implements Comparable<coupon> {
-    int[] combination;
+
+    List<Integer> combination = new ArrayList<> ( );
     int val;
     int threshold;
+    int id;
 
-    public coupon ( int[] combination , int val , int threshold ) {
+    public coupon ( ) {
+    }
+
+    public coupon ( int id , List<Integer> combination , int val , int threshold ) {
+        this.id = id;
         this.combination = combination;
         this.val = val;
         this.threshold = threshold;
@@ -46,13 +50,20 @@ class coupon implements Comparable<coupon> {
         return false;
     }
 
+    public void cloneCoupon ( coupon other,List<Integer> list ) {
+        this.id = other.id;
+        this.threshold = other.threshold;
+        this.val = other.val;
+        this.combination=list;
+    }
 
     @Override
     public String toString ( ) {
         return "coupon{" +
-                "combination=" + Arrays.toString ( combination ) +
+                "combination=" + combination +
                 ", val=" + val +
                 ", threshold=" + threshold +
+                ", id=" + id +
                 '}';
     }
 
@@ -68,7 +79,7 @@ public class Main {
         return random.nextInt ( max - min ) + min;
     }
 
-    public static final int NUMITEMS = 99;
+    public static final int NUMITEMS = 10;
     public static final int NUMCOUPONS = 1000;
 
     public static void main ( String[] args ) {
@@ -84,14 +95,16 @@ public class Main {
             items[i] = new item ( i , getRandomNumber ( MAXPRICE , MINPRICE ) , getRandomNumber ( MAXCOUNT , 1 ) );
         }
         //set coupons;
-        coupon[] coupons=new coupon[NUMCOUPONS];
-        for(int i =0; i<NUMCOUPONS;i++){
-            int[] combs = new int[getRandomNumber(NUMITEMS,1)];
-            for(int j=0;j<combs.length;j++){
-                combs[j]=(getRandomNumber(NUMITEMS,1));
+        coupon[] coupons = new coupon[NUMCOUPONS];
+        for ( int i = 0 ; i < NUMCOUPONS ; i++ ) {
+            List<Integer> combs = new ArrayList<> ( );
+            for ( int j = 0 ; j < getRandomNumber ( NUMITEMS , 1 ) ; j++ ) {
+                combs.add ( getRandomNumber ( NUMITEMS , 1 ) );
             }
-
-            coupons[i]=new coupon(combs,getRandomNumber(DISCOUNT,1),getRandomNumber(100,1));
+            combs = combs.stream ( )
+                    .distinct ( )
+                    .collect ( Collectors.toList ( ) );
+            coupons[i] = new coupon ( i , combs , getRandomNumber ( DISCOUNT , 1 ) , getRandomNumber ( 100 , 1 ) );
         }
 //        for(int i=0;i<NUMITEMS;i++) {
 //            System.out.println(items[i].toString());
@@ -112,24 +125,22 @@ public class Main {
 //        coupons[3] = new coupon ( a8 , 6 , 10 );
 //        coupons[4] = new coupon ( a5 , 50 , 14 );
 
-//        for(int i=0;i<NUMCOUPONS;i++) {
-//            System.out.println(coupons[i].toString());
-//        }
+        for(int i=0;i<NUMCOUPONS;i++) {
+            System.out.println(coupons[i].toString());
+        }
 
-        HashSet<coupon> validCoupon = new HashSet<> ( );
-        couponOptimization ( items , coupons , validCoupon );
-        coupon[] coup = new coupon[validCoupon.size()];
-        validCoupon.toArray (coup);
-        Arrays.sort ( coup );
+        List<coupon> validCoupon = new ArrayList<> ( );
+        couponRangeShrink ( items , coupons , validCoupon );
+        //Arrays.sort ( validCoupon );
 //        Iterator i = validCoupon.iterator ();
 //        while(i.hasNext ()){
 //            System.out.println(i.next ().toString ());
 //        }
-        for(coupon x: coup){
-            System.out.println ( x.toString () );
+        for ( coupon x : validCoupon ) {
+            System.out.println ( x.toString ( ) );
         }
-        int result = getBestDiscount ( coup );
-        System.out.println ( result );
+//        int result = getBestDiscount ( coup );
+//        System.out.println ( result );
     }
 
     public static void addTOSet ( int[] comb , HashSet<Integer> set ) {
@@ -147,54 +158,69 @@ public class Main {
         return false;
     }
 
-
-    public static void couponOptimization ( item[] items , coupon[] coupons , HashSet<coupon> validCoupon ) {
-        //check if the coupon has the item included in its combination.
-        for ( int i = 0 ; i < items.length ; i++ ) {
-            for ( int j = 0 ; j < coupons.length ; j++ ) {
-                //if the item is included, check if it meets the price threshold.
-                if ( coupons[j].checkValidity ( items[i].id ) && !validCoupon.contains ( coupons[j] )) {
-                    if ( items[i].val >= coupons[j].threshold ) {
-                        validCoupon.add ( coupons[j] );
-                    }
-                    else {
-                        int sum = items[i].val;
-                        for (int u = i+1;u<items.length;u++ ){
-                            if(coupons[j].checkValidity ( items[u].id )){
-                                if(sum+items[u].val>=coupons[j].threshold){
-                                    validCoupon.add(coupons[j]);
-                                    break;
-                                }
-                                else{
-                                    sum=+items[u].val;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public static int getBestDiscount ( coupon[] coupons ) {
-        int bestDiscount = 0;
+    public static void couponRangeShrink ( item[] items , coupon[] coupons , List<coupon> validCoupon ) {
         for ( int i = 0 ; i < coupons.length ; i++ ) {
-            int temp = coupons[i].val;
-            HashSet<Integer> set = new HashSet<> ( );
-            addTOSet ( coupons[i].combination , set );
-            for ( int j = i + 1 ; j < coupons.length ; j++ ) {
-                if ( ! isOverlapping ( coupons[j].combination , set ) ) {
-                    addTOSet ( coupons[j].combination , set );
-                    temp += coupons[j].val;
+            List<Integer> newList = new ArrayList<> ( );
+            coupon temp = new coupon ( );
+            for ( int j = 0 ; j < items.length ; j++ ) {
+                if ( coupons[i].checkValidity ( items[j].id ) ) {
+                    newList.add(items[j].id);
                 }
-//                System.out.println ( "base on coupon " + i + " " + coupons[i].toString ( ) +
-//                        " calculating best combination with coupon " + j + " " + coupons[j].toString ( )
-//                        + "current best offer is " + temp );
             }
-            bestDiscount = Math.max ( temp , bestDiscount );
-
+            if(newList.size()>0){
+                temp.cloneCoupon ( coupons[i],newList );
+                validCoupon.add(temp);
+            }
         }
-        return bestDiscount;
     }
+
+//    public static void couponOptimization ( item[] items , coupon[] coupons , HashSet<coupon> validCoupon ) {
+//        //check if the coupon has the item included in its combination.
+//        for ( int i = 0 ; i < items.length ; i++ ) {
+//            for ( int j = 0 ; j < coupons.length ; j++ ) {
+//                //if the item is included, check if it meets the price threshold.
+//                if ( coupons[j].checkValidity ( items[i].id ) && !validCoupon.contains ( coupons[j] )) {
+//                    if ( items[i].val >= coupons[j].threshold ) {
+//                        validCoupon.add ( coupons[j] );
+//                    }
+//                    else {
+//                        int sum = items[i].val;
+//                        for (int u = i+1;u<items.length;u++ ){
+//                            if(coupons[j].checkValidity ( items[u].id )){
+//                                if(sum+items[u].val>=coupons[j].threshold){
+//                                    validCoupon.add(coupons[j]);
+//                                    break;
+//                                }
+//                                else{
+//                                    sum=+items[u].val;
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+//    public static int getBestDiscount ( coupon[] coupons ) {
+//        int bestDiscount = 0;
+//        for ( int i = 0 ; i < coupons.length ; i++ ) {
+//            int temp = coupons[i].val;
+//            HashSet<Integer> set = new HashSet<> ( );
+//            addTOSet ( coupons[i].combination , set );
+//            for ( int j = i + 1 ; j < coupons.length ; j++ ) {
+//                if ( ! isOverlapping ( coupons[j].combination , set ) ) {
+//                    addTOSet ( coupons[j].combination , set );
+//                    temp += coupons[j].val;
+//                }
+////                System.out.println ( "base on coupon " + i + " " + coupons[i].toString ( ) +
+////                        " calculating best combination with coupon " + j + " " + coupons[j].toString ( )
+////                        + "current best offer is " + temp );
+//            }
+//            bestDiscount = Math.max ( temp , bestDiscount );
+//
+//        }
+//        return bestDiscount;
+//    }
 
 }
